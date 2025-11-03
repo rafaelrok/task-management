d6ab5b2dbfe1bf868e6d45dea2e67b792da9f3b5
𝗥𝗔𝗙𝗔𝗘𝗟𝗩𝗜𝗘𝗜𝗥𝗔.🅳🅴🆅 <rafaelvieirasantos2006@hotmail.com>
2025-11-03 20:11:20 -0300

    Implementado estrutura de pasta e pre-configuracao de test containers para subir o postgresql com docker

---
+++ src/test/java/br/com/rafaelvieira/task_management/config/TestContainersConfig.java
@@ -0,0 +1,21 @@
+package br.com.rafaelvieira.task_management.config;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestContainersConfig {
+
+    @Bean
+    @ServiceConnection
+    public PostgreSQLContainer<?> postgresContainer() {
+        return new PostgreSQLContainer<>(DockerImageName.parse("postgres:latest"))
+                .withDatabaseName("testdb")
+                .withUsername("test")
+                .withPassword("test")
+                .withReuse(true);
+    }
+}
